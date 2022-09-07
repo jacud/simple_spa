b7d102ac72fe108e6bae76771fb63e1c966c0d05
Askar Zaripov <zaripov.a@openregion.info>
2022-09-07 13:51:04 +0500

    added infinity scroll

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,33 +1,34 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import PostItem from "./PostItem";
 
-const PostList  = ({posts, title, deletePost}) => { 
-    if(posts.length === 0) {
-        return (
-            <h2 style={{textAlign:'center'}}>Nothing found</h2>
-        );
-    }
+const PostList  = forwardRef(({posts, title, deletePost}, ref) => { 
     return (
         <div className="post_list">
             <div className="post_list_header">
                 <h1>{title}</h1>
             </div>
             
-            <TransitionGroup>                
-                {posts.map((post, index) =>
-                    <CSSTransition
-                        key={post.id}
-                        timeout={500}
-                        classNames="post"
-                    >
-                        <PostItem post={post} index={post.id} onDelete={deletePost}/>
-                    </CSSTransition>
-                )}
-            </TransitionGroup>
+            { 
+            posts.length ? <TransitionGroup>                
+                    {posts.map((post, index) =>
+                        <CSSTransition
+                            key={post.id}
+                            timeout={500}
+                            classNames="post"
+                        >
+                            <PostItem post={post} index={post.id} onDelete={deletePost}/>
+                        </CSSTransition>
+                    )}
+                </TransitionGroup>
+                : <h2 style={{textAlign:'center'}}>Nothing found</h2>
+            }
+
+
+            <div ref={ref} style={{visibility: 'hidden', height: '0px', width: '100%', background: 'red'}}></div>
             
         </div>
     );
-}
+})
 
 export default PostList;
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import PostService from "../API/PostService";
 import Pagination from "../components/Pagination";
 import PostFilter from "../components/PostFilter";
@@ -7,7 +7,9 @@ import PostList from "../components/PostList";
 import StyledButton from "../components/UI/button/StyledButton";
 import Loader from "../components/UI/loader/Loader";
 import Modal from "../components/UI/modal/Modal";
+import StyledSelect from "../components/UI/select/StyledSelect";
 import { useFetching } from "../hooks/useFetching";
+import { useObserver } from "../hooks/useObserver";
 import { usePosts } from "../hooks/usePosts"; 
 
 import { getPagesCount } from "../utils/pages";
@@ -19,15 +21,21 @@ export default function Posts() {
   const [totalPages, setTotalPages] = useState(1);
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
+  const lastElementRef = useRef();
+
   const [fetchPosts, isPostLoading, postError] = useFetching(async (limit, page) => {
-    const {data: posts, headers} = await PostService.getAll(limit, page);
-    setPosts(posts);
+    const {data: newPosts, headers} = await PostService.getAll(limit, page);
+    setPosts([...posts, ...newPosts]);
     setTotalPages(getPagesCount(headers['x-total-count'], limit));
   });
 
+  useObserver(lastElementRef, page < totalPages, isPostLoading, () => {
+    setPage(prev => prev + 1);
+  })
+
   useEffect(() => {
     fetchPosts(limit, page);
-  }, []);
+  }, [page, limit]);
 
   const deletePost = (post) => {
     setPosts(posts.filter(item => item.id !== post.id)); 
@@ -40,10 +48,10 @@ export default function Posts() {
     setModalVisibility(false);
   }
 
-  const selectPage = (page) => {
-    setPage(page);
-    fetchPosts(limit, page);
-  }
+  // const selectPage = (page) => {
+  //   setPage(page);
+  //   fetchPosts(limit, page);
+  // }
 
   return (
     <div className="App">
@@ -60,20 +68,34 @@ export default function Posts() {
         setFilter={setFilter}
       />
       <hr />
+      <StyledSelect 
+        value={limit}
+        defaultValue={"Количество элементов на странице"}
+        options={
+            [
+                {value: "5", title: "5"},
+                {value: "10", title: "10"},
+                {value: "15", title: "15"},
+                {value: "20", title: "20"},
+                {value: "-1", title: "Все"}
+            ]
+        }
+        onSelect={selectedLimit => setLimit(selectedLimit)}
+      />
+      
       { 
         postError && 
         <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
             <h1>SomeError {postError}</h1>
         </div>
       }
-      <PostList deletePost={deletePost} posts={sortedAndFilteredPosts} title={"Список постов"}/>
+      <PostList ref={lastElementRef} deletePost={deletePost} posts={sortedAndFilteredPosts} title={"Список постов"}/>
       {
         isPostLoading  && <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
           <Loader />
-        </div> 
-
+        </div>
       }
-      <Pagination selected={page} totalPages={totalPages} onSelect={(p) => selectPage(p)} />     
+      {/* <Pagination selected={page} totalPages={totalPages} onSelect={(p) => selectPage(p)} /> */}
     </div>
   )
 }
